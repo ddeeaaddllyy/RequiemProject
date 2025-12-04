c616dc8eacce99d41e082dbb37dd7526ef346d2d
ddeeaaddllyy <naumova070783@gmail.com>
2025-12-04 22:39:36 +0300

    there u can change tabs

---
+++ app/src/main/java/com/application/requiemproject/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.application.requiemproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity: AppCompatActivity() {
+
+    private val homeFragment = HomeFragment()
+    private val searchFragment = SearchFragment()
+    private val profileFragment = ProfileFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        replaceFragment(homeFragment)
+
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    replaceFragment(homeFragment)
+                    true
+                }
+                R.id.navigation_search -> {
+                    replaceFragment(searchFragment)
+                    true
+                }
+                R.id.navigation_profile -> {
+                    replaceFragment(profileFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
+    }
+}
