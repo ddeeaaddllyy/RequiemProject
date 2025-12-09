34d4fc3df96f4bd6581ec483e4a0b4e9d28cb407
ddeeaaddllyy <naumova070783@gmail.com>
2025-12-09 22:30:43 +0300

    I made an intermediate step to create the search fragment logic

--- app/src/main/java/com/application/requiemproject/SearchFragment.kt
+++ app/src/main/java/com/application/requiemproject/SearchFragment.kt
@@ -1,17 +1,41 @@
 package com.application.requiemproject
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val x = "123"
+        val searchInput = view.findViewById<EditText>(R.id.input_search)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_search_results)
 
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        searchInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val query = s.toString()
+
+                if (query.isNotEmpty()) {
+                    performSearch(query)
+                }
+            }
+        })
     }
+
+    private fun performSearch(query: String) {
+        // логика фильтрации списка вопросов
+    }
+
 }
