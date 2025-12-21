c8d29c5fa00965d095d6b839b741339822d099e1
ddeeaaddllyy <naumova070783@gmail.com>
2025-12-21 23:41:56 +0300

    User data class for room

---
+++ app/src/main/java/com/application/requiemproject/data/local/entities/User.kt
@@ -0,0 +1,21 @@
+package com.application.requiemproject.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+
+    var login: String,
+
+    var email: String? = null,
+
+    var password: String,
+
+    // 0 - just user without premium
+    // 1 - user with premium
+    // 2 - moderator (idk why it is needed)
+    var privilege: Short = 0
+)
