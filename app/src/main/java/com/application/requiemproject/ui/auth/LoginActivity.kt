f47f2ef60eb15886ba6c3386c282a1da62f5545f
ddeeaaddllyy <naumova070783@gmail.com>
2025-12-21 23:45:55 +0300

    created first normal working version of the main login activity

--- app/src/main/java/com/application/requiemproject/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/application/requiemproject/ui/auth/LoginActivity.kt
@@ -3,15 +3,21 @@ package com.application.requiemproject.ui.auth
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.application.requiemproject.MainActivity
 import com.application.requiemproject.R
+import com.application.requiemproject.data.local.db.AppDatabase
 import com.google.android.material.textfield.TextInputEditText
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val db = AppDatabase.getDatabase(this)
 
         val buttonLogin = findViewById<Button>(R.id.button_login)
         val buttonRegister = findViewById<Button>(R.id.button_register)
@@ -19,13 +25,31 @@ class LoginActivity : AppCompatActivity() {
         val inputPassword = findViewById<TextInputEditText>(R.id.input_password)
 
         buttonLogin.setOnClickListener {
-            val enteredEmail = inputLogin.text.toString().trim()
+            val enteredLogin = inputLogin.text.toString().trim()
             val enteredPassword = inputPassword.text.toString().trim()
 
-            if (true) {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
+            if (enteredLogin.isEmpty() || enteredPassword.isEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Сначала заполните все поля",
+                    Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            lifecycleScope.launch {
+                val user = db.userDao().getUserByLogin(enteredLogin)
+
+                if (user != null && enteredPassword == user.password) {
+                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+
+                } else {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Неверный логин или пароль",
+                        Toast.LENGTH_SHORT).show()
+                }
             }
         }
 
